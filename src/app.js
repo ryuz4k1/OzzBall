365f8708949fb65d96f444dd7dc01bbadab07d86
ryuz4k1 <ozerr.murat@hotmail.com>
2019-12-02 17:33:18 +0300

    project structure created

---
+++ src/app.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const express                   = require("express");
+const bodyParser                = require("body-parser");
+
+const path                      = require('path');
+
+// db Connection
+const Connection                = require('../src/helpers/connection');
+
+//Controllers
+
+// Middleware
+
+//Config
+const config                    = require('../config.json');
+
+
+
+class App {
+  constructor() {
+      this.app = express();
+
+      this.config();
+      this.controllers();
+
+      this.Connection = new Connection().mongoDB();
+  }
+
+  config() {
+    //db connection
+    this.Connection;
+
+    // ... Set app port
+    this.app.set("PORT", config.app.port);
+    
+    // ... Body parser
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json()); //json tipinde gelicek post datalarını karşılar
+
+    // view engine setup
+    this.app.set('views', path.join(__dirname, 'views'));
+    this.app.set('view engine', 'jade');
+
+    //config
+    this.app.set('apiKey', config.apiKey.key);
+
+  }
+
+  controllers(){
+    this.app.use("/public", express.static("public"));
+    
+  };
+
+  getApp() {
+    return this.app;
+  }
+
+}
+
+module.exports = App;
