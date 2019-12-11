ab938e97a427b7f094653812e9ccbd0e1f317ff4
ryuz4k1 <ozerr.murat@hotmail.com>
2019-12-11 14:58:16 +0300

    home page icon

--- public/javascripts/controllers/homeController.js
+++ public/javascripts/controllers/homeController.js
@@ -3,7 +3,7 @@ app.controller('homeController', ["$scope", 'homeFactory', ($scope, homeFactory)
     // ... Get username
     var btn = document.getElementById('myButton');
     btn.addEventListener('click', () => {
-        homeFactory.getUsername().then(data => {
+        homeFactory.getUsername().then( data => {
             window.document.location = '/game?username='+data
         });
     });
